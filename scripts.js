cff4af112393c17ecb8964ed7460e791551d2d9b
Tom Allen <tomallen851@yahoo.com>
2021-02-08 18:37:00 -0600

    add CSS and add index, script

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// grab all of the palette divs and use
+const paletteDivs = document.querySelectorAll("#palette div");
+
+const paletteStyles = getComputedStyle(paletteDivs[0]);
+
+paletteDivs.forEach((paletteDiv, index) => {
+  paletteDiv.style.setProperty(
+    "background-color",
+    getComputedStyle(paletteDiv).getPropertyValue(`--${index}`)
+  );
+  console.log(getComputedStyle(paletteDiv).getPropertyValue("--0"));
+});
